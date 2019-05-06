e5eabb70933f2f2d8d258c006368e4751e0376a8
jiangxiaocai <jiangxiaocai@cryptogorilla.io>
2019-05-06 15:49:38 +0800

    new img

--- pure_node_web/README.md
+++
@@ -1,8 +0,0 @@
-api + web页面。
-
-纯node 实现的，web页面 + 服务端接口。 
-项目启动方法：
-```
-node server.js
-```
-浏览器打开: http://localhost:3001
--- test/index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type':'text/plain'});
-	res.end(JSON.stringify({userName:'zhangsan'}));
-}).listen(3389);
-
-console.log('runing: http://localhost:3389/ => userName:zhangsan');

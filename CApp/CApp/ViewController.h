b87c32c9eed810f438e3bba418527b920bb6be77
mythkiven <mythziven@gmail.com>
2019-07-25 21:13:25 +0800

    add icon

---
+++ CApp/CApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CApp
+//
+//  Created by apple on 2019/7/25.
+//  Copyright © 2019 MrthKiven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CApp/CApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CApp
+//
+//  Created by apple on 2019/7/25.
+//  Copyright © 2019 MrthKiven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
